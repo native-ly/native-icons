a9bfceeb065380c0a4895bca6861e27cf6098252
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-26 10:43:05 +0100

    feat: load fonts on demand

--- src/helpers/getIconType.ts
+++ src/helpers/getIconType.ts
@@ -1,65 +1,50 @@
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import Entypo from 'react-native-vector-icons/Entypo'
-import EvilIcons from 'react-native-vector-icons/EvilIcons'
-import Feather from 'react-native-vector-icons/Feather'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
-import Fontisto from 'react-native-vector-icons/Fontisto'
-import Foundation from 'react-native-vector-icons/Foundation'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import Octicons from 'react-native-vector-icons/Octicons'
-import Zocial from 'react-native-vector-icons/Zocial'
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
-
 import type { FontType } from '../types/FontType'
 
 export const getIconType = (type?: FontType) => {
   switch (type) {
     case 'ant-design':
-      return AntDesign
+      return require('react-native-vector-icons/AntDesign').default
 
     case 'entypo':
-      return Entypo
+      return require('react-native-vector-icons/Entypo').default
 
     case 'evil-icons':
-      return EvilIcons
+      return require('react-native-vector-icons/EvilIcons').default
 
     case 'feather':
-      return Feather
+      return require('react-native-vector-icons/Feather').default
 
     case 'font-awesome':
-      return FontAwesome
+      return require('react-native-vector-icons/FontAwesome').default
 
     case 'font-awesome-5':
-      return FontAwesome5
+      return require('react-native-vector-icons/FontAwesome5').default
 
     case 'fontisto':
-      return Fontisto
+      return require('react-native-vector-icons/Fontisto').default
 
     case 'foundation':
-      return Foundation
+      return require('react-native-vector-icons/Foundation').default
 
     case 'ionicons':
-      return Ionicons
+      return require('react-native-vector-icons/Ionicons').default
 
     case 'material-icons':
-      return MaterialIcons
+      return require('react-native-vector-icons/MaterialIcons').default
 
     case 'material-community-icons':
-      return MaterialCommunityIcons
+      return require('react-native-vector-icons/MaterialCommunityIcons').default
 
     case 'octicons':
-      return Octicons
+      return require('react-native-vector-icons/Octicons').default
 
     case 'zocial':
-      return Zocial
+      return require('react-native-vector-icons/Zocial').default
 
     case 'simple-line-icons':
-      return SimpleLineIcons
+      return require('react-native-vector-icons/SimpleLineIcons').default
 
     default:
-      return MaterialIcons
+      return require('react-native-vector-icons/MaterialIcons').default
   }
 }
